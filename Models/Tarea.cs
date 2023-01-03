94e5130c3ab3f74b2ae2311ddcebe25c0cfacac2
Rodolfo Maximo <rodomaxi2@gmail.com>
2023-01-03 16:58:53 -0600

    Se agregan atributos, anotaciones y se intenta crear conexcion a db

--- Models/Tarea.cs
+++ Models/Tarea.cs
@@ -1,11 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace EFProject.Models;
 
 public class Tarea
 {
+    [Key]
     public Guid TareaId {get;set;}
 
+    [ForeignKey("CategoriaId")]
     public Guid CategoriaId {get;set;}
 
+    [Required]
+    [MaxLength(200)]
     public string Titulo {get;set;}
 
     public string Description {get;set;}
@@ -15,6 +22,9 @@ public class Tarea
     public DateTime FechaCreacion {get;set;}
 
     public virtual Categoria Categoria {get;set;}
+
+    [NotMapped]
+    public string Resumen {get;set;}
 }
 
 public enum Prioridad
--- Program.cs
+++ Program.cs
@@ -1,6 +1,21 @@
+using EFProject;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// builder.Services.AddDbContext<TareasContext>( p => p.UseInMemoryDatabase("TareasDB"));
+
+builder.Services.AddSqlServer<TareasContext>("Data Source=(localhost\\DESKTOP-NSCNOG7);Initial Catalog=TareasDb;Trusted_Connection=True; Integrated Security=True");
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/dbconn", async ([FromServices] TareasContext dbContext) =>
+{
+    dbContext.Database.EnsureCreated();
+    return Results.Ok("Base de datos en memoria: " + dbContext.Database.IsInMemory());
+});
 
 app.Run();
