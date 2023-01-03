2197b5babb789bb8411c464d65ddb370b77c01fe
Rodolfo Maximo <rodomaxi2@gmail.com>
2023-01-03 13:44:47 -0600

    Primer commit del proyecto

---
+++ Models/Categoria.cs
@@ -0,0 +1,12 @@
+namespace EFProject.Models;
+
+public class Categoria 
+{
+    public Guid CategoriaId {get;set;}
+
+    public string Nombre {get;set;}
+
+    public string Description {get;set;}
+
+    public virtual ICollection<Tarea> Tareas {get;set;}
+}
